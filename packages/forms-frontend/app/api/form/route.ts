1c29cfa0bedc3e078a5853d4921da7cb6d11b967
npham49 <brian.d.pham@gov.bc.ca>
2023-06-22 16:32:33 -0700

    added submissions table and nextjs api route for tracking submissions

--- packages/forms-frontend/app/api/form/route.ts
+++ packages/forms-frontend/app/api/form/route.ts
@@ -1,22 +1,41 @@
+/* eslint-disable import/prefer-default-export */
 import { PrismaClient } from "@prisma/client"
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
 const prisma = new PrismaClient()
 
-export default async function POST(request: Request) {
-    const req = await request.json()
-    const res = await prisma.submission.create({
-        data: {
-            email: req.body.email
-        }
-    })
-
-    return NextResponse.json(res, {
-        status: 200,
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "POST",
-            "Access-Control-Allow-Headers": "Content-Type"
-        }
-    })
+export async function POST(req: NextRequest, res: NextResponse) {
+    try {
+        const json = await req.json()
+        if (!json.email) throw new Error("Email is required")
+        const existing = await prisma.email.findUnique({
+            where: {
+                email: json.email
+            }
+        })
+        if (!existing) throw new Error("Email is not part of the trial")
+        const resp = await prisma.submission.create({
+            data: {
+                email: json.email
+            }
+        })
+        return NextResponse.json(resp, {
+            status: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "POST",
+                "Access-Control-Allow-Headers": "Content-Type"
+            }
+        })
+    } catch (error: any) {
+        console.log(error)
+        return NextResponse.json(error.message, {
+            status: 500,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "POST",
+                "Access-Control-Allow-Headers": "Content-Type"
+            }
+        })
+    }
 }
