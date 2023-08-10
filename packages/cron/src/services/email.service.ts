9a05e440a11ec4b6dcfc276047023b9c292be6f9
Trista <74997356+trista-d@users.noreply.github.com>
2023-08-10 10:35:26 -0700

    Update sender email

--- packages/cron/src/services/email.service.ts
+++ packages/cron/src/services/email.service.ts
@@ -94,7 +94,7 @@ const sendEmail = async (chesToken: string, recipient: Email): Promise<AxiosResp
             priority: "normal",
             bodyType: "html", // "html" or "text"
             subject: `Connect with WorkBC Employment Services (${recipient.template} template)`,
-            from: "brian.d.pham@gov.bc.ca",
+            from: "sdpr.elmsdtechnical@gov.bc.ca",
             body
         }
         const sendEmailResult: AxiosResponse = await chesApi.post("api/v1/email", req, {
