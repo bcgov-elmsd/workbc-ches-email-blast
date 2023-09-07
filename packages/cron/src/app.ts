e72708bf098782b3361228c3570f065101c46ac6
Trista <trista.dhami@gov.bc.ca>
2023-09-07 09:37:51 -0700

    update comment to reflect cron time

--- packages/cron/src/app.ts
+++ packages/cron/src/app.ts
@@ -28,7 +28,7 @@ app.get("/", (req: Express.Request, res: any) => {
 })
 
 // Cron job to schedule email delivery
-// between 8AM to 10PM PST, every 5 minutes, send 60 emails
+// between 8AM to 6PM PST, every 5 minutes, send 60 emails
 // Note: maximum recommended usage is 60 emails per minute
 cronService.startJob("* */5 * 8-18 * 1-5", emailController.getAndSendEmail)
 
