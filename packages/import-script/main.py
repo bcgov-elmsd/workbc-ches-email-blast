b71a647d2e573699a9af657ba24d3e2092c98fbc
npham49 <brian.d.pham@gov.bc.ca>
2023-09-11 11:55:39 -0700

    added logging on import script

--- packages/import-script/main.py
+++ packages/import-script/main.py
@@ -16,10 +16,11 @@ def connect():
 	conn = psycopg2.connect(host=os.getenv("DB_HOST"),database=os.getenv("DB_NAME"),port=os.getenv("DB_PORT"),user=os.getenv("DB_USER"),password=os.getenv("DB_PASSWORD"))
 	cur = conn.cursor()
 	cur.execute('SELECT * FROM "Email"')
-	with open(sys.argv[1], 'r') as f:
-		reader = csv.reader(f)
+	with open(sys.argv[1], 'r', encoding="utf8") as f:
+		reader = csv.reader(f, dialect=csv.excel)
 		next(reader)
 		for row in reader:
+			print("importing "+row[EMAIL])
 			cur.execute('INSERT INTO "Email" VALUES (default, %s ,%s, %s, default, default, default, %s, %s)', (row[UID],row[EMAIL], row[CONDITION],row[FIRST_NAME]+" "+row[LAST_NAME],row[CATCHMENT].split()[0]))
 	conn.commit()
 	conn.close()
